060368154976f796d4cfd80346e1c48f2ea1aaa4
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-01-18 09:09:38 +0300

    monty

--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
 #ifndef MONTY_H
-#define MONTY_H#
+#define MONTY_H
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -9,36 +9,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
-
-/* struct 2*/
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
 
 /**
  * struct global_s - variables -> args, file, line
@@ -58,7 +28,37 @@ typedef struct global_s
 
 extern glob_t glob;
 
-/* prototypes*/
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Function prototypes */
 void execute_file(stack_t **stack);
 void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number);
 
@@ -79,9 +79,10 @@ void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
 void f_rotl(stack_t **head, unsigned int counter);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+
 /* Stack helper functions */
 stack_t *add_node(stack_t **stack, const int n);
 int is_number(char *str);
 void free_stack(stack_t *stack);
 
-#endif
+#endif /* monty.h */
